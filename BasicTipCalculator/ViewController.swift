b9b819470329fc382feadf023dbb9b6269dc7d5c
Rout, Saroj <srout@mtvl140e6441f.local>
2017-03-08 07:36:26 -0800

    Initial Commit

---
+++ BasicTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicTipCalculator
+//
+//  Created by Rout, Saroj on 3/8/17.
+//  Copyright © 2017 Rout, Saroj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
